21a944868f2c3a7de563e0b4224bdddf0b526a38
XJP-GIT <77379590+xjp-git@users.noreply.github.com>
2021-04-27 10:41:02 +0800

    Add macOS env config #8

--- bin/init/env.sh
+++ bin/init/env.sh
@@ -5,6 +5,8 @@ if [ -z "$ANDROID_HOME" ]; then
     export ANDROID_HOME="$HOME/Android/Sdk"
   elif [ -d "$HOME/.local/lib/android/sdk" ]; then
     export ANDROID_HOME="$HOME/.local/lib/android/sdk"
+  elif [ -d "$HOME/Library/Android/sdk" ]; then
+    export ANDROID_HOME="$HOME/Library/Android/sdk"
   fi
 fi
 
@@ -14,13 +16,23 @@ _NDK="$ANDROID_HOME/ndk/21.4.7075529"
 [ -f "$_NDK/source.properties" ] || _NDK="$ANDROID_HOME/ndk-bundle"
 
 if [ ! -f "$_NDK/source.properties" ]; then
+  read -p "Enter your NDK version: " NDK_VERSION
+  _NDK="$ANDROID_HOME/ndk/$NDK_VERSION"
+fi
+
+if [ ! -f "$_NDK/source.properties" ]; then
   echo "Error: NDK not found."
   exit 1
 fi
 
 export ANDROID_NDK_HOME=$_NDK
 export NDK=$_NDK
-export PROJECT=$(realpath .)
+
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+  export PROJECT=$PWD
+else
+  export PROJECT=$(realpath .)
+fi
 
 if [ ! $(command -v go) ]; then
   if [ -d /usr/lib/go-1.16 ]; then
