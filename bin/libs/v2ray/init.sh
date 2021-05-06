e2764f5347389be227c69fc1af3fb3c048ae2182
世界 <sekai@neko.services>
2021-05-06 23:06:36 +0800

    Fail if checkout failed

--- bin/libs/v2ray/init.sh
+++ bin/libs/v2ray/init.sh
@@ -6,7 +6,7 @@ export GO111MOUDLE=on
 export PATH="$PATH:$(go env GOPATH)/bin"
 
 cd $PROJECT
-[ -f v2ray/go.mod ] || git submodule update --init v2ray
+[ -f v2ray/go.mod ] || git submodule update --init v2ray || exit 1
 cd v2ray
 git reset --hard && git clean -fdx
 go mod download -x && go get -v golang.org/x/mobile/cmd/... || exit 1
