551f31b102e26c1d12554b60558e9246cc993439
世界 <sekai@neko.services>
2021-05-17 13:26:12 +0800

    Split naive build

---
+++ bin/libs/naive/x86_64.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source "bin/init/env.sh"
+
+ROOT="$PROJECT/naive-plugin/src/main/jniLibs"
+OUTPUT="naive"
+LIB_OUTPUT="lib$OUTPUT.so"
+
+git submodule update --init --recursive 'naive-plugin/*'
+cd naive-plugin/src/main/jni/naiveproxy/src
+
+export EXTRA_FLAGS='target_os="android" target_cpu="x64"'
+./get-clang.sh
+./build.sh
+DIR="$ROOT/x86_64"
+rm -rf $DIR
+mkdir -p $DIR
+cp out/Release/naive $DIR/$LIB_OUTPUT
