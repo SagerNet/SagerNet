d39569266d80e3d75c44cfec381e5f06f8bd88b5
世界 <sekai@neko.services>
2021-05-18 14:04:47 +0800

    Build naive with installed ndk

--- bin/libs/naive.sh
+++ bin/libs/naive.sh
@@ -9,6 +9,10 @@ LIB_OUTPUT="lib$OUTPUT.so"
 git submodule update --init --recursive 'naive-plugin/*'
 cd naive-plugin/src/main/jni/naiveproxy/src
 
+rm third_party/android_ndk
+ln -s $NDK third_party/android_ndk
+[ -f third_party/android_ndk/BUILD.gn ] || cp ../../BUILD.gn third_party/android_ndk
+
 export EXTRA_FLAGS='target_os="android" target_cpu="arm"'
 ./get-clang.sh
 ./build.sh
@@ -40,3 +44,5 @@ DIR="$ROOT/x86_64"
 rm -rf $DIR
 mkdir -p $DIR
 cp out/Release/naive $DIR/$LIB_OUTPUT
+
+rm third_party/android_ndk
--- bin/libs/naive/arm64-v8a.sh
+++ bin/libs/naive/arm64-v8a.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 source "bin/init/env.sh"
-
-ROOT="$PROJECT/naive-plugin/src/main/jniLibs"
-OUTPUT="naive"
-LIB_OUTPUT="lib$OUTPUT.so"
-
-git submodule update --init --recursive 'naive-plugin/*'
-cd naive-plugin/src/main/jni/naiveproxy/src
+source "bin/libs/naive/build.sh"
 
 export EXTRA_FLAGS='target_os="android" target_cpu="arm64"'
 ./get-clang.sh
