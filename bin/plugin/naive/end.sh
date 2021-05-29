a4ec21ccab40284dcf1adcb740c16f0d8e2517b9
世界 <sekai@neko.services>
2021-05-29 22:50:58 +0800

    Revert "Build naive with installed ndk"

--- bin/plugin/naive/end.sh
+++ bin/plugin/naive/end.sh
@@ -3,5 +3,5 @@
 source "bin/init/env.sh"
 source "bin/plugin/naive/build.sh"
 
-rm -f third_party/android_ndk
-rm -f $NDK/BUILD.gn
+#rm -f third_party/android_ndk
+#rm -f $NDK/BUILD.gn
--- bin/plugin/naive/init.sh
+++ bin/plugin/naive/init.sh
@@ -8,6 +8,6 @@ CURR_PATH="$PROJECT/$CURR"
 git submodule update --init --recursive "$CURR/*"
 cd $CURR_PATH/src/main/jni/naiveproxy/src
 
-rm -f third_party/android_ndk
-ln -s $NDK third_party/android_ndk
-[ -f third_party/android_ndk/BUILD.gn ] || cp ../../BUILD.gn third_party/android_ndk
+#rm -f third_party/android_ndk
+#ln -s $NDK third_party/android_ndk
+#[ -f third_party/android_ndk/BUILD.gn ] || cp ../../BUILD.gn third_party/android_ndk
