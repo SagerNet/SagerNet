4c015dde348379c82d6796d59361c494981c670f
世界 <sekai@neko.services>
2021-07-19 22:52:19 +0800

    Remove unused proxychains

--- app/src/main/java/io/nekohasekai/sagernet/bg/Executable.kt
+++ app/src/main/java/io/nekohasekai/sagernet/bg/Executable.kt
@@ -33,18 +33,19 @@ object Executable {
     const val SS_LOCAL = "libsslocal.so"
     const val SSR_LOCAL = "libssr-local.so"
     const val TUN2SOCKS = "libtun2socks.so"
-    const val PROXYCHAINS = "libproxychains4.so"
 
     private val EXECUTABLES = setOf(SS_LOCAL, SSR_LOCAL, TUN2SOCKS)
 
     fun killAll() {
         for (process in File("/proc").listFiles { _, name -> TextUtils.isDigitsOnly(name) }
             ?: return) {
-            val exe = File(try {
-                File(process, "cmdline").inputStream().bufferedReader().readText()
-            } catch (_: IOException) {
-                continue
-            }.split(Character.MIN_VALUE, limit = 2).first())
+            val exe = File(
+                try {
+                    File(process, "cmdline").inputStream().bufferedReader().readText()
+                } catch (_: IOException) {
+                    continue
+                }.split(Character.MIN_VALUE, limit = 2).first()
+            )
             if (EXECUTABLES.contains(exe.name)) try {
                 Os.kill(process.name.toInt(), OsConstants.SIGKILL)
                 Logs.w("SIGKILL ${exe.nameWithoutExtension} (${process.name}) succeed")
