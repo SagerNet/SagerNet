d24c8520851fea1fb40e3f186320cc51456fa360
世界 <i@sekai.icu>
2021-09-05 17:15:27 +0800

    Remove dependencies on grpc

--- bin/update_core.sh
+++ bin/update_core.sh
@@ -6,8 +6,8 @@ git reset origin/main --hard
 popd
 
 pushd external/v2ray-core
-git fetch origin master || exit 1
-git reset origin/master --hard
+git fetch origin main || exit 1
+git reset origin/main --hard
 popd
 
 git add .
--- library/proto-stub/build.gradle.kts
+++ library/proto-stub/build.gradle.kts
@@ -8,47 +8,23 @@ plugins {
 
 setupKotlinCommon()
 
-val grpcVersion = "1.40.1"
-val grpcKotlinVersion = "1.1.0"
 val protobufVersion = "3.17.3"
 
 dependencies {
     protobuf(project(":library:proto"))
 
     api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
-    api("io.grpc:grpc-protobuf-lite:$grpcVersion")
-    api("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
-    api("io.grpc:grpc-stub:$grpcVersion")
-    api("com.google.protobuf:protobuf-javalite:$protobufVersion")
+    api("com.google.protobuf:protobuf-java:$protobufVersion")
 }
 
 protobuf {
     protoc {
         artifact = "com.google.protobuf:protoc:$protobufVersion"
     }
-    plugins {
-        id("java") {
-            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
-        }
-        id("grpc") {
-            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
-        }
-        id("grpckt") {
-            artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk7@jar"
-        }
-    }
     generateProtoTasks {
         all().forEach {
             it.plugins {
-                id("java") {
-                    option("lite")
-                }
-                id("grpc") {
-                    option("lite")
-                }
-                id("grpckt") {
-                    option("lite")
-                }
+                create("java")
             }
         }
     }
