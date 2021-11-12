31e9cda9826ea6e7d527514f932777e9f859dc5c
世界 <i@sekai.icu>
2021-11-12 15:37:07 +0800

    Update core

--- bin/update_core.sh
+++ bin/update_core.sh
@@ -6,8 +6,8 @@ git reset origin/main --hard
 popd
 
 pushd external/v2ray-core
-git fetch origin dev-v5main || exit 1
-git reset origin/dev-v5main --hard
+git fetch origin main || exit 1
+git reset origin/main --hard
 popd
 
 git add .
