6b41c083ff7c09d975638e4dc5adfd5596b66944
世界 <i@sekai.icu>
2021-12-14 13:59:45 +0800

    Fix naive

--- app/src/main/java/io/nekohasekai/sagernet/fmt/naive/NaiveFmt.kt
+++ app/src/main/java/io/nekohasekai/sagernet/fmt/naive/NaiveFmt.kt
@@ -44,7 +44,7 @@ fun parseNaive(link: String): NaiveBean {
 }
 
 fun NaiveBean.toUri(proxyOnly: Boolean = false): String {
-    val builder = linkBuilder().host(finalAddress).port(finalPort)
+    val builder = linkBuilder().host(serverAddress).port(finalPort)
     if (username.isNotBlank()) {
         builder.username(username)
         if (password.isNotBlank()) {
