0e65f439c76db91adf2239008c6570f9695e5063
世界 <i@sekai.icu>
2021-12-16 10:37:32 +0800

    Remove f-droid abi flavors #328

--- buildSrc/src/main/kotlin/Helpers.kt
+++ buildSrc/src/main/kotlin/Helpers.kt
@@ -348,21 +348,7 @@ fun Project.setupPlugin(projectName: String) {
         flavorDimensions("vendor")
         productFlavors {
             create("oss")
-            create("fdroidArm64") {
-                versionNameSuffix = "-arm64"
-            }
-            create("fdroidArm") {
-                versionCode = verCode - 1
-                versionNameSuffix = "-arm"
-            }
-            create("fdroidX64") {
-                versionCode = verCode - 2
-                versionNameSuffix = "-x64"
-            }
-            create("fdroidX86") {
-                versionCode = verCode - 3
-                versionNameSuffix = "-x86"
-            }
+            create("fdroid")
             create("play") {
                 versionCode = verCode - 4
             }
@@ -467,21 +453,7 @@ fun Project.setupApp() {
         productFlavors {
             create("oss")
             create("expert")
-            create("fdroidArm64") {
-                versionNameSuffix = "-arm64"
-            }
-            create("fdroidArm") {
-                versionCode = verCode - 1
-                versionNameSuffix = "-arm"
-            }
-            create("fdroidX64") {
-                versionCode = verCode - 2
-                versionNameSuffix = "-x64"
-            }
-            create("fdroidX86") {
-                versionCode = verCode - 3
-                versionNameSuffix = "-x86"
-            }
+            create("fdroid")
             create("play") {
                 versionCode = verCode - 4
             }
