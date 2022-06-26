e4fa0cc979f2c2a6b674de7d39f938431f67e05c
世界 <i@sekai.icu>
2022-06-26 19:52:01 +0800

    Update plugin build action

--- bin/fdroid/prebuild.sh
+++ bin/fdroid/prebuild.sh
@@ -5,7 +5,7 @@ source "bin/init/env.sh"
 git submodule update --init 'external/*'
 git submodule update --init 'library/*'
 
-bin/fdroid/install_golang.sh 1.18.2
+bin/fdroid/install_golang.sh 1.18.3
 
 echo "sdk.dir=$ANDROID_HOME" >>local.properties
 echo "ndk.dir=$ANDROID_NDK_HOME" >>local.properties
---
+++ bin/init/action/rust.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rustup target add armv7-linux-androideabi aarch64-linux-android i686-linux-android x86_64-linux-android
