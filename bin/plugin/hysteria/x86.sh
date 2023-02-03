e458da9624deeb6d3d661ec3f15582c6f5abc717
Binary Craft <elliot.huang.signed@gmail.com>
2023-02-03 16:38:11 +0800

    Update hysteria

--- bin/plugin/hysteria/x86.sh
+++ bin/plugin/hysteria/x86.sh
@@ -5,4 +5,4 @@ source "bin/plugin/hysteria/build.sh"
 
 DIR="$ROOT/x86"
 mkdir -p $DIR
-env CC=$ANDROID_X86_CC GOARCH=386 go build -v -o $DIR/$LIB_OUTPUT -trimpath -ldflags "-s -w -buildid=" ./cmd
+env CC=$ANDROID_X86_CC GOARCH=386 go build -v -o $DIR/$LIB_OUTPUT -trimpath -ldflags "-s -w -buildid=" ./app/cmd
--- bin/plugin/hysteria/x86_64.sh
+++ bin/plugin/hysteria/x86_64.sh
@@ -5,5 +5,5 @@ source "bin/plugin/hysteria/build.sh"
 
 DIR="$ROOT/x86_64"
 mkdir -p $DIR
-env CC=$ANDROID_X86_64_CC GOARCH=amd64 go build -v -o $DIR/$LIB_OUTPUT -trimpath -ldflags "-s -w -buildid=" ./cmd
+env CC=$ANDROID_X86_64_CC GOARCH=amd64 go build -v -o $DIR/$LIB_OUTPUT -trimpath -ldflags "-s -w -buildid=" ./app/cmd
 
