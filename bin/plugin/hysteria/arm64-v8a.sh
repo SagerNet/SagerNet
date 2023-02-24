7952c52a8f8685c8629122f51ec7692518da0c54
binarycraft007 <107379182+binarycraft007@users.noreply.github.com>
2023-02-25 06:42:13 +0800

    Update hysteria (#632)

--- bin/plugin/hysteria/arm64-v8a.sh
+++ bin/plugin/hysteria/arm64-v8a.sh
@@ -5,4 +5,4 @@ source "bin/plugin/hysteria/build.sh"
 
 DIR="$ROOT/arm64-v8a"
 mkdir -p $DIR
-env CC=$ANDROID_ARM64_CC GOARCH=arm64 go build -v -o $DIR/$LIB_OUTPUT -trimpath -ldflags "-s -w -buildid=" ./cmd
+env CC=$ANDROID_ARM64_CC GOARCH=arm64 go build -v -o $DIR/$LIB_OUTPUT -trimpath -ldflags "-s -w -buildid=" ./app/cmd
--- bin/plugin/hysteria/armeabi-v7a.sh
+++ bin/plugin/hysteria/armeabi-v7a.sh
@@ -5,4 +5,4 @@ source "bin/plugin/hysteria/build.sh"
 
 DIR="$ROOT/armeabi-v7a"
 mkdir -p $DIR
-env CC=$ANDROID_ARM_CC GOARCH=arm GOARM=7 go build -v -o $DIR/$LIB_OUTPUT -trimpath -ldflags "-s -w -buildid=" ./cmd
+env CC=$ANDROID_ARM_CC GOARCH=arm GOARM=7 go build -v -o $DIR/$LIB_OUTPUT -trimpath -ldflags "-s -w -buildid=" ./app/cmd
